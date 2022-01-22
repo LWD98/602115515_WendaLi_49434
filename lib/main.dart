14365a44617f4367416ebef58c8221d10501f22c
wenda <dnf1158561536@gmail.com>
2022-01-22 16:32:37 +0800

    parsing background

---
+++ lib/main.dart
@@ -0,0 +1,104 @@
+// ignore_for_file: file_names
+
+import 'dart:async';
+import 'dart:convert';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class Photo {
+  final int albumId;
+  final int id;
+  final String title;
+  final String url;
+  final String thumbnailUrl;
+
+  const Photo({
+    required this.albumId,
+    required this.id,
+    required this.title,
+    required this.url,
+    required this.thumbnailUrl,
+  });
+
+  factory Photo.fromJson(Map<String, dynamic> json) {
+    return Photo(
+      albumId: json['albumId'] as int,
+      id: json['id'] as int,
+      title: json['title'] as String,
+      url: json['url'] as String,
+      thumbnailUrl: json['thumbnailUrl'] as String,
+    );
+  }
+}
+
+Future<List<Photo>> fetchPhotos(http.Client client) async {
+  final response = await client
+      .get(Uri.parse('https://jsonplaceholder.typicode.com/photos'));
+  print(response.body);
+  print('computer: = ${compute(parsePhotos, response.body)}');
+
+  return compute(parsePhotos, response.body);
+}
+
+List<Photo> parsePhotos(String responseBody) {
+  final parsed = jsonDecode(responseBody).cast<Map<String, dynamic>>();
+
+  return parsed.map<Photo>((json) => Photo.fromJson(json)).toList();
+}
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'flutter demo',
+      theme: ThemeData(primarySwatch: Colors.amber),
+      home: Myhome(title: 'flutter API'),
+    );
+  }
+}
+
+class Myhome extends StatefulWidget {
+  Myhome({Key? key, required String title}) : super(key: key);
+
+  @override
+  _MyhomeState createState() => _MyhomeState();
+}
+
+class _MyhomeState extends State<Myhome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('flutter'),
+      ),
+      body: FutureBuilder<List<Photo>>(
+        future: fetchPhotos(http.Client()),
+        builder: (context, snapshpt) {
+          if (snapshpt.hasError) {
+            return const Center(
+              child: Text('Error'),
+            );
+          } else if (snapshpt.hasData) {
+            return ListView.builder(
+              itemCount: snapshpt.data!.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(snapshpt.data![index].title),
+                );
+              },
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
